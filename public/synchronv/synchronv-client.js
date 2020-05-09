d3bbcc9120f84191cc72b4a23985739129d8e8a5
ruccho <noboroo2000@yahoo.co.jp>
2020-05-09 23:31:32 +0900

    videoの再生同期を追加

--- public/synchronv/synchronv-client.js
+++ public/synchronv/synchronv-client.js
@@ -1,26 +1,124 @@
 
-function Synchronv(socketHost) {
+function Synchronv(socketHost, video) {
 
+    //socket setup
     var socket = io(socketHost)
     this.socket = socket;
 
+    this.sessionId = "";
+
     socket.on('connect', () => {
         console.log('connect');
-        socket.emit('join', {});
+        var joinData = {};
+        if(this.sessionId != "") joinData.session_id = this.sessionId;
+        socket.emit('join', joinData);
     });
 
     socket.on('participants_changed', (data) => {
         console.log("participants_changed");
         console.log(data);
+
+        if (this.sessionId == "") {
+            this.sessionId = data.session_id;
+        }
+
         if (this._onParticipantsChanged !== void 0)
             this._onParticipantsChanged(data.count);
     });
 
     socket.on('disconnect', () => {
         console.log('disconnected');
+        //this.sessionId = "";
+    });
+
+    //video control setup
+
+    this.video = video;
+    this.isSeeking = false;
+    this.isInSeekingOperation = false;
+    this.sessionId = "";
+
+    video.addEventListener("seeked", () => {
+        if (this.isInSeekingOperation && this.isSeeking) {
+            this.isSeeking = false;
+            this.socket.emit('ready_to_play', {
+                session_id: this.sessionId
+            });
+        }
+    });
+
+    socket.on('control_seek', (data) => {
+        console.log("control_seek");
+        if (!this.isSeeking) {
+            this._beginSeekingOperation();
+            this.isSeeking = true;
+            this.video.pause();
+            this.video.currentTime = data.position;
+        }
+    });
+
+    socket.on('complete_seek', (data) => {
+        console.log("complete_seek");
+        console.log("autoplay: " + data.autoplay);
+        if (this.isInSeekingOperation) {
+            this._endSeekingOperation();
+            this.isSeeking = false;
+            if (data.autoplay)
+            {
+                this.video.play();
+            }
+        }
     });
 }
 
 Synchronv.prototype.onParticipantsChanged = function (callback) {
     this._onParticipantsChanged = callback;
+}
+
+Synchronv.prototype.sendPlay = function () {
+    if (!this.isInSeekingOperation) {
+        this._beginSeekingOperation();
+        this.video.pause();
+        this.socket.emit('request_seek', {
+            session_id: this.sessionId,
+            position: this.video.currentTime,
+            autoplay: true
+        });
+    }
+}
+
+Synchronv.prototype.sendPause = function () {
+    if (!this.isInSeekingOperation) {
+        this._beginSeekingOperation();
+        this.video.pause();
+        this.socket.emit('request_seek', {
+            session_id: this.sessionId,
+            position: this.video.currentTime,
+            autoplay: false
+        });
+    }
+}
+
+Synchronv.prototype.sendSeek = function (position) {
+    if (!this.isInSeekingOperation) {
+        this._beginSeekingOperation();
+        let autoplay = !video.paused;
+        this.video.pause();
+        this.socket.emit('request_seek', {
+            session_id: this.sessionId,
+            position: position,
+            autoplay: autoplay
+        });
+    }
+}
+
+Synchronv.prototype._beginSeekingOperation = function(){
+    this.isInSeekingOperation = true;
+    if (this.onOperationStateChanged !== void 0)
+        this.onOperationStateChanged(false);
+}
+Synchronv.prototype._endSeekingOperation = function(){
+    this.isInSeekingOperation = false;
+    if (this.onOperationStateChanged !== void 0)
+        this.onOperationStateChanged(true);
 }
--- routes/live.js
+++ routes/live.js
@@ -3,18 +3,23 @@ var router = express.Router();
 
 var uuid = require('uuid');
 
+const seekingTimeout = 5000;
+
 var liveSession = {
   id: undefined,
-  participants: []
+  participants: [],
+  isInSeekingOperation: false,
+  seekingControlMessageCount: 0,
+  seekingReadyToPlayCount: 0,
+  seekingAutoPlay: false,
+  seekingPosition: 0,
+  //最後にcomplete_seekを発信した時刻。新規参加者のJoin時、再生位置を推定するのに使用。
+  seekingCompletedTime: 0,
+  //最後にシーク同期を開始した時刻。タイムアウトの判定に使用。
+  seekingStartedTime: 0
 };
 
 liveSession.id = uuid.v4().split('-').join('');
-
-/*
-router.get('/test', function (req, res, next) {
-  res.render('live/test', { live_session: liveSession });
-});
-*/
 
 //WebSocket setup
 
@@ -29,25 +34,64 @@ socketio.on('connection', function (socket) {
 
   socket.on("disconnect", () => onSocketDisconnect(socket));
 
-  socket.on("join", () => onSocketJoin(socket));
+  socket.on("join", (data) => onSocketJoin(socket, data));
+
+  socket.on("request_seek", (data) => onSocketRequestSeek(socket, data));
+
+  socket.on("ready_to_play", (data) => onSocketReadyToPlay(socket, data));
 
 });
+
+//参加者管理
 
 function onSocketJoin(socket, data) {
 
   console.log("New participant joined.");
+  //セッションIDのチェック
+  if (data.session_id !== void 0 && data.session_id != liveSession.id) {
+    console.log("Invalid session ID.");
+    return;
+  }
 
   var participant = liveSession.participants.find(p => p.socket == socket);
   if (participant === void 0) {
-    console.log("Participant registered.");
     //未参加
     participant = {
-      socket: socket
+      socket: socket,
+      readyToPlay: false
     };
     liveSession.participants.push(participant);
 
+    console.log("Participant registered.");
     console.log(liveSession);
     notifyParticipantsChanged();
+    console.log("liveSession.seekingAutoPlay: " + liveSession.seekingAutoPlay)
+    //シーク位置の同期
+    if (liveSession.isInSeekingOperation) {
+      //すでにシーク操作中の場合
+
+      participant.socket.emit("control_seek",
+        {
+          session_id: liveSession.id,
+          position: liveSession.seekingPosition
+        });
+
+    } else {
+      //再生中の場合
+      //現在位置を算出
+      var passed = (Date.now() - liveSession.seekingCompletedTime) * 0.001;
+
+      if (liveSession.seekingAutoPlay) {
+        //再生中
+        startSeek(liveSession.seekingPosition + passed, true);
+      } else {
+        //停止中
+        startSeek(liveSession.seekingPosition, false);
+      }
+
+    }
+
+
   } else {
     //すでに参加中
     console.log("This participant has already joined.");
@@ -73,11 +117,103 @@ function onSocketDisconnect(socket) {
 }
 
 function notifyParticipantsChanged() {
-  socketio.emit('participants_changed',
+  liveSession.participants.forEach((participant, index) => {
+    participant.socket.emit('participants_changed',
+      {
+        session_id: liveSession.id,
+        count: liveSession.participants.length
+      });
+  });
+}
+
+//シーク同期
+
+function onSocketRequestSeek(socket, data) {
+  console.log("New seekng request has been received.");
+  //セッションIDのチェック
+  if (data.session_id != liveSession.id) {
+    console.log("Invalid session ID.");
+    return;
+  }
+  startSeek(data.position, data.autoplay);
+}
+
+function startSeek(position, autoplay) {
+
+  if (liveSession.isInSeekingOperation) {
+    //別のシークリクエストの処理中は弾く
+    console.log("There is another seeking request being processed.");
+    return;
+  }
+
+  liveSession.isInSeekingOperation = true;
+  liveSession.seekingAutoPlay = autoplay;
+  liveSession.seekingPosition = position;
+  liveSession.participants.forEach((participant, index) => {
+    participant.readyToPlay = false;
+    participant.socket.emit("control_seek",
+      {
+        session_id: liveSession.id,
+        position: position
+      });
+  });
+  
+  //タイムアウトの設定
+  liveSession.seekingStartedTime = Date.now();
+  setTimeout(() =>{
+    if(Date.now() - liveSession.seekingStartedTime >= seekingTimeout)
     {
-      session_id: liveSession.id,
-      count: liveSession.participants.length
-    });
+      completeSeek();
+    }
+  }, seekingTimeout + 100)
+
+  console.log(liveSession.participants.length + " control_request have been sent. autoplay: " + autoplay);
+}
+
+function onSocketReadyToPlay(socket, data) {
+  //セッションIDのチェック
+  if (data.session_id != liveSession.id) {
+    console.log("Invalid session ID.");
+    return;
+  }
+
+  //シーク同期処理中でなければ弾く
+  if (!liveSession.isInSeekingOperation) return;
+
+  var participant = liveSession.participants.find((p) => p.socket == socket);
+  if (participant === void 0) return;
+
+  participant.readyToPlay = true;
+
+  var participantsCount = liveSession.participants.length;
+  var readyToPlayCount = 0;
+  liveSession.participants.forEach((participant, index) => {
+    if (participant.readyToPlay) readyToPlayCount++;
+  });
+
+  console.log("Ready to play: " + readyToPlayCount + "/" + participantsCount);
+
+  //control_seekを行った全クライアントがready_to_playを返した
+  if (readyToPlayCount == participantsCount) {
+    completeSeek();
+  }
+}
+
+function completeSeek()
+{
+  //シーク同期処理中でなければ弾く
+  if (!liveSession.isInSeekingOperation) return;
+  
+  liveSession.isInSeekingOperation = false;
+  liveSession.seekingCompletedTime = Date.now();
+  console.log("Broadcast complete_seek. autoplay: " + liveSession.seekingAutoPlay);
+  liveSession.participants.forEach((participant, index) => {
+    participant.socket.emit("complete_seek",
+      {
+        session_id: liveSession.id,
+        autoplay: liveSession.seekingAutoPlay
+      });
+  });
 }
 
 module.exports = router;
